c348ea7e293b30d34e190a4aa3980ddb4a2ace98
tony <a155268747@gmail.com>
2021-06-09 15:41:00 +0800

    bug fix

--- skillcalc.js
+++ skillcalc.js
@@ -1,4 +1,4 @@
-/*V1.2*/
+/*V1.4*/
 /*---------------UI---------------*/
 var table = document.querySelector("#dataTables");
 var div = document.createElement("div");
@@ -80,111 +80,79 @@ function update() {
     sw[1] = u2msw && u2ssw ? (u2msw + u2ssw) / 2 : u2msw || u2ssw;
     sw[2] = u3msw && u3ssw ? (u3msw + u3ssw) / 2 : u3msw || u3ssw;
     //sw_max
-    sw_max[0] = document.querySelector("#U1max").value ? Number(document.querySelector("#U1max").value) / 100 : 1;
-    sw_max[1] = document.querySelector("#U2max").value ? Number(document.querySelector("#U2max").value) / 100 : 1;
-    sw_max[2] = document.querySelector("#U3max").value ? Number(document.querySelector("#U3max").value) / 100 : 1;
+    sw_max[0] = document.querySelector("#U1max").value ? document.querySelector("#U1max").value / 100 : 1;
+    sw_max[1] = document.querySelector("#U2max").value ? document.querySelector("#U2max").value / 100 : 1;
+    sw_max[2] = document.querySelector("#U3max").value ? document.querySelector("#U3max").value / 100 : 1;
     //skillsw
-    var U1skillsw1 = Array.from(document.querySelectorAll(".U1skillsw1")).map(e => e.value ? Number(e.value) : 0);
+    var U1skillsw1 = Array.from(document.querySelectorAll(".U1skillsw1")).map(e => e.value ? e.value : 0);
     var U1skillsw_counter1 = Array.from(document.querySelectorAll(".U1skillsw-counter1")).map(e => e.value ? Number(e.value) : 0);
-    var U1skillsw2 = Array.from(document.querySelectorAll(".U1skillsw2")).map(e => e.value ? Number(e.value) : 0);
+    var U1skillsw2 = Array.from(document.querySelectorAll(".U1skillsw2")).map(e => e.value ? e.value : 0);
     var U1skillsw_counter2 = Array.from(document.querySelectorAll(".U1skillsw-counter2")).map(e => e.value ? Number(e.value) : 0);
-    var U1skillsw3 = Array.from(document.querySelectorAll(".U1skillsw3")).map(e => e.value ? Number(e.value) : 0);
+    var U1skillsw3 = Array.from(document.querySelectorAll(".U1skillsw3")).map(e => e.value ? e.value : 0);
     var U1skillsw_counter3 = Array.from(document.querySelectorAll(".U1skillsw-counter3")).map(e => e.value ? Number(e.value) : 0);
-    var U2skillsw1 = Array.from(document.querySelectorAll(".U2skillsw1")).map(e => e.value ? Number(e.value) : 0);
+    var U2skillsw1 = Array.from(document.querySelectorAll(".U2skillsw1")).map(e => e.value ? e.value : 0);
     var U2skillsw_counter1 = Array.from(document.querySelectorAll(".U2skillsw-counter1")).map(e => e.value ? Number(e.value) : 0);
-    var U2skillsw2 = Array.from(document.querySelectorAll(".U2skillsw2")).map(e => e.value ? Number(e.value) : 0);
+    var U2skillsw2 = Array.from(document.querySelectorAll(".U2skillsw2")).map(e => e.value ? e.value : 0);
     var U2skillsw_counter2 = Array.from(document.querySelectorAll(".U2skillsw-counter2")).map(e => e.value ? Number(e.value) : 0);
-    var U2skillsw3 = Array.from(document.querySelectorAll(".U2skillsw3")).map(e => e.value ? Number(e.value) : 0);
+    var U2skillsw3 = Array.from(document.querySelectorAll(".U2skillsw3")).map(e => e.value ? e.value : 0);
     var U2skillsw_counter3 = Array.from(document.querySelectorAll(".U2skillsw-counter3")).map(e => e.value ? Number(e.value) : 0);
-    var U3skillsw1 = Array.from(document.querySelectorAll(".U3skillsw1")).map(e => e.value ? Number(e.value) : 0);
+    var U3skillsw1 = Array.from(document.querySelectorAll(".U3skillsw1")).map(e => e.value ? e.value : 0);
     var U3skillsw_counter1 = Array.from(document.querySelectorAll(".U3skillsw-counter1")).map(e => e.value ? Number(e.value) : 0);
-    var U3skillsw2 = Array.from(document.querySelectorAll(".U3skillsw2")).map(e => e.value ? Number(e.value) : 0);
+    var U3skillsw2 = Array.from(document.querySelectorAll(".U3skillsw2")).map(e => e.value ? e.value : 0);
     var U3skillsw_counter2 = Array.from(document.querySelectorAll(".U3skillsw-counter2")).map(e => e.value ? Number(e.value) : 0);
-    var U3skillsw3 = Array.from(document.querySelectorAll(".U3skillsw3")).map(e => e.value ? Number(e.value) : 0);
+    var U3skillsw3 = Array.from(document.querySelectorAll(".U3skillsw3")).map(e => e.value ? e.value : 0);
     var U3skillsw_counter3 = Array.from(document.querySelectorAll(".U3skillsw-counter3")).map(e => e.value ? Number(e.value) : 0);
     //skillsw to list
-    if (U1skillsw1.length && U1skillsw1.length == U1skillsw_counter1.length) {
-        U1skillsw1.forEach((e, i) => { if (e) { U1skillsw.push([1, e, U1skillsw_counter1[i]]) } });
-    }
-    if (U1skillsw2.length && U1skillsw2.length == U1skillsw_counter2.length) {
-        U1skillsw2.forEach((e, i) => { if (e) { U2skillsw.push([1, e, U1skillsw_counter2[i]]) } });
-    }
-    if (U1skillsw3.length && U1skillsw3.length == U1skillsw_counter3.length) {
-        U1skillsw3.forEach((e, i) => { if (e) { U3skillsw.push([1, e, U1skillsw_counter3[i]]) } });
-    }
-    if (U2skillsw1.length && U2skillsw1.length == U2skillsw_counter1.length) {
-        U2skillsw1.forEach((e, i) => { if (e) { U1skillsw.push([2, e, U2skillsw_counter1[i]]) } });
-    }
-    if (U2skillsw2.length && U2skillsw2.length == U2skillsw_counter2.length) {
-        U2skillsw2.forEach((e, i) => { if (e) { U2skillsw.push([2, e, U2skillsw_counter2[i]]) } });
-    }
-    if (U2skillsw3.length && U2skillsw3.length == U2skillsw_counter3.length) {
-        U2skillsw3.forEach((e, i) => { if (e) { U3skillsw.push([2, e, U2skillsw_counter3[i]]) } });
-    }
-    if (U3skillsw1.length && U3skillsw1.length == U3skillsw_counter1.length) {
-        U3skillsw1.forEach((e, i) => { if (e) { U1skillsw.push([3, e, U3skillsw_counter1[i]]) } });
-    }
-    if (U3skillsw2.length && U3skillsw2.length == U3skillsw_counter2.length) {
-        U3skillsw2.forEach((e, i) => { if (e) { U2skillsw.push([3, e, U3skillsw_counter2[i]]) } });
-    }
-    if (U3skillsw3.length && U3skillsw3.length == U3skillsw_counter3.length) {
-        U3skillsw3.forEach((e, i) => { if (e) { U3skillsw.push([3, e, U3skillsw_counter3[i]]) } });
-    }
+    U1skillsw1.forEach((e, i) => { if (e) { U1skillsw.push([0, e, U1skillsw_counter1[i]]) } });
+    U1skillsw2.forEach((e, i) => { if (e) { U2skillsw.push([0, e, U1skillsw_counter2[i]]) } });
+    U1skillsw3.forEach((e, i) => { if (e) { U3skillsw.push([0, e, U1skillsw_counter3[i]]) } });
+    U2skillsw1.forEach((e, i) => { if (e) { U1skillsw.push([1, e, U2skillsw_counter1[i]]) } });
+    U2skillsw2.forEach((e, i) => { if (e) { U2skillsw.push([1, e, U2skillsw_counter2[i]]) } });
+    U2skillsw3.forEach((e, i) => { if (e) { U3skillsw.push([1, e, U2skillsw_counter3[i]]) } });
+    U3skillsw1.forEach((e, i) => { if (e) { U1skillsw.push([2, e, U3skillsw_counter1[i]]) } });
+    U3skillsw2.forEach((e, i) => { if (e) { U2skillsw.push([2, e, U3skillsw_counter2[i]]) } });
+    U3skillsw3.forEach((e, i) => { if (e) { U3skillsw.push([2, e, U3skillsw_counter3[i]]) } });
     //skillacc
-    var U1skillacc_rate = Array.from(document.querySelectorAll(".U1skillacc")).map(e => e.value ? Number(e.value) : 0);
-    var U1skillacc_max = Array.from(document.querySelectorAll(".U1skillacc-max")).map(e => e.value ? Number(e.value) : 0);
-    var U2skillacc_rate = Array.from(document.querySelectorAll(".U2skillacc")).map(e => e.value ? Number(e.value) : 0);
-    var U2skillacc_max = Array.from(document.querySelectorAll(".U2skillacc-max")).map(e => e.value ? Number(e.value) : 0);
-    var U3skillacc_rate = Array.from(document.querySelectorAll(".U3skillacc")).map(e => e.value ? Number(e.value) : 0);
-    var U3skillacc_max = Array.from(document.querySelectorAll(".U3skillacc-max")).map(e => e.value ? Number(e.value) : 0);
-    if (U1skillacc_rate.length && U1skillacc_rate.length == U1skillacc_max.length) {
-        U1skillacc_rate.forEach((e, i) => { if (e) { U1skillacc.push([e / 100, U1skillacc_max[i] / 100]) } });
-    }
-    if (U2skillacc_rate.length && U2skillacc_rate.length == U2skillacc_max.length) {
-        U2skillacc_rate.forEach((e, i) => { if (e) { U2skillacc.push([e / 100, U2skillacc_max[i] / 100]) } });
-    }
-    if (U3skillacc_rate.length && U3skillacc_rate.length == U3skillacc_max.length) {
-        U3skillacc_rate.forEach((e, i) => { if (e) { U3skillacc.push([e / 100, U3skillacc_max[i] / 100]) } });
-    }
+    var U1skillacc_rate = Array.from(document.querySelectorAll(".U1skillacc")).map(e => e.value ? e.value : 0);
+    var U1skillacc_max = Array.from(document.querySelectorAll(".U1skillacc-max")).map(e => e.value ? e.value : 0);
+    var U2skillacc_rate = Array.from(document.querySelectorAll(".U2skillacc")).map(e => e.value ? e.value : 0);
+    var U2skillacc_max = Array.from(document.querySelectorAll(".U2skillacc-max")).map(e => e.value ? e.value : 0);
+    var U3skillacc_rate = Array.from(document.querySelectorAll(".U3skillacc")).map(e => e.value ? e.value : 0);
+    var U3skillacc_max = Array.from(document.querySelectorAll(".U3skillacc-max")).map(e => e.value ? e.value : 0);
+    U1skillacc_rate.forEach((e, i) => { if (e) { U1skillacc.push([e / 100, U1skillacc_max[i] / 100]) } });
+    U2skillacc_rate.forEach((e, i) => { if (e) { U2skillacc.push([e / 100, U2skillacc_max[i] / 100]) } });
+    U3skillacc_rate.forEach((e, i) => { if (e) { U3skillacc.push([e / 100, U3skillacc_max[i] / 100]) } });
     //exboost
     var U1_exboost_time = Array.from(document.querySelectorAll(".U1exboost-time")).map(e => e.value ? Number(e.value) : 0);
-    var U1_exboost_charge = Array.from(document.querySelectorAll(".U1exboost-charge")).map(e => e.value ? Number(e.value) : 0);
+    var U1_exboost_charge = Array.from(document.querySelectorAll(".U1exboost-charge")).map(e => e.value ? e.value : 0);
     var U2_exboost_time = Array.from(document.querySelectorAll(".U2exboost-time")).map(e => e.value ? Number(e.value) : 0);
-    var U2_exboost_charge = Array.from(document.querySelectorAll(".U2exboost-charge")).map(e => e.value ? Number(e.value) : 0);
+    var U2_exboost_charge = Array.from(document.querySelectorAll(".U2exboost-charge")).map(e => e.value ? e.value : 0);
     var U3_exboost_time = Array.from(document.querySelectorAll(".U3exboost-time")).map(e => e.value ? Number(e.value) : 0);
-    var U3_exboost_charge = Array.from(document.querySelectorAll(".U3exboost-charge")).map(e => e.value ? Number(e.value) : 0);
+    var U3_exboost_charge = Array.from(document.querySelectorAll(".U3exboost-charge")).map(e => e.value ? e.value : 0);
     //exboost to list
-    if (U1_exboost_time.length && U1_exboost_time.length == U1_exboost_charge.length) {
-        U1_exboost_time.forEach((e, i) => { if (e) { exboost.push([e, 1, U1_exboost_charge[i]]); } });
-    }
-    if (U2_exboost_time.length && U2_exboost_time.length == U2_exboost_charge.length) {
-        U2_exboost_time.forEach((e, i) => { if (e) { exboost.push([e, 2, U2_exboost_charge[i]]); } });
-    }
-    if (U3_exboost_time.length && U3_exboost_time.length == U3_exboost_charge.length) {
-        U3_exboost_time.forEach((e, i) => { if (e) { exboost.push([e, 3, U3_exboost_charge[i]]); } });
-    }
-    if (exboost.length) {
-        exboost.sort((a, b) => a[0] - b[0]);
-    }
+    U1_exboost_time.forEach((e, i) => { if (e) { exboost.push([e, 0, U1_exboost_charge[i] / 100]); } });
+    U2_exboost_time.forEach((e, i) => { if (e) { exboost.push([e, 1, U2_exboost_charge[i] / 100]); } });
+    U3_exboost_time.forEach((e, i) => { if (e) { exboost.push([e, 2, U3_exboost_charge[i] / 100]); } });
+    exboost.sort((a, b) => a[0] - b[0]);
 }
 
 function timeline() {
-    if (!(sw[0] && sw[1] && sw[2])) { return; }
+    sw.forEach(e => { if (!e) { e = 100000; } });
     //init
     eventlist = [];
     var curCharge = [0, 0, 0];
     var skillcounter = [0, 0, 0];
     var currentPoint = 0;
     var nextPoint = 0;
-    curCharge[0] = document.querySelector("#U1start").value ? Math.min(sw[0] * Number(document.querySelector("#U1start").value) / 100, sw_max[0] * sw[0]) : 0;
-    curCharge[1] = document.querySelector("#U2start").value ? Math.min(sw[1] * Number(document.querySelector("#U2start").value) / 100, sw_max[1] * sw[1]) : 0;
-    curCharge[2] = document.querySelector("#U3start").value ? Math.min(sw[2] * Number(document.querySelector("#U3start").value) / 100, sw_max[2] * sw[2]) : 0;
+    curCharge[0] = document.querySelector("#U1start").value ? Math.min(sw[0] * document.querySelector("#U1start").value / 100, sw_max[0] * sw[0]) : 0;
+    curCharge[1] = document.querySelector("#U2start").value ? Math.min(sw[1] * document.querySelector("#U2start").value / 100, sw_max[1] * sw[1]) : 0;
+    curCharge[2] = document.querySelector("#U3start").value ? Math.min(sw[2] * document.querySelector("#U3start").value / 100, sw_max[2] * sw[2]) : 0;
 
     function nextTimePoint() {
         if (sw[0] > curCharge[0] && sw[1] > curCharge[1] && sw[2] > curCharge[2]) {
             var next_sw = Math.min((sw[0] - curCharge[0]) * 100 / (skillacc[0] * 100), (sw[1] - curCharge[1]) * 100 / (skillacc[1] * 100), (sw[2] - curCharge[2]) * 100 / (skillacc[2] * 100));
             if (exboost.length) {
-                return Math.ceil(currentPoint + Math.min(next_sw, exboost[0][0]));
+                return Math.ceil(Math.min(currentPoint + next_sw, exboost[0][0]));
             } else {
                 return Math.ceil(currentPoint + next_sw);
             }
@@ -198,7 +166,7 @@ function timeline() {
             curCharge[0] -= sw[0];
             U1skillsw.forEach(e => {
                 if (e[2] == 0 || skillcounter[0] < e[2]) {
-                    curCharge[e[0] - 1] = curCharge[e[0] - 1] + e[1] * sw[e[0] - 1] / 100;
+                    curCharge[e[0]] = curCharge[e[0]] + e[1] * sw[e[0]] / 100;
                 }
             })
             skillcounter[0] += 1;
@@ -210,7 +178,7 @@ function timeline() {
             curCharge[1] -= sw[1];
             U2skillsw.forEach(e => {
                 if (e[2] == 0 || skillcounter[1] < e[2]) {
-                    curCharge[e[0] - 1] = curCharge[e[0] - 1] + e[1] * sw[e[0] - 1] / 100;
+                    curCharge[e[0]] = curCharge[e[0]] + e[1] * sw[e[0]] / 100;
                 }
             })
             skillcounter[1] += 1;
@@ -222,7 +190,7 @@ function timeline() {
             curCharge[2] -= sw[2];
             U3skillsw.forEach(e => {
                 if (e[2] == 0 || skillcounter[2] < e[2]) {
-                    curCharge[e[0] - 1] = curCharge[e[0] - 1] + e[1] * sw[e[0] - 1] / 100;
+                    curCharge[e[0]] = curCharge[e[0]] + e[1] * sw[e[0]] / 100;
                 }
             })
             skillcounter[2] += 1;
@@ -234,7 +202,7 @@ function timeline() {
 
     function settleCharge() {
         while (exboost.length && currentPoint == exboost[0][0]) {
-            curCharge[exboost[0][1] - 1] += exboost[0][2] / 100 * sw[exboost[0][1] - 1];
+            curCharge[exboost[0][1]] += exboost[0][2] * sw[exboost[0][1]];
             exboost.shift();
         }
         curCharge[0] = Math.min(sw_max[0] * sw[0], curCharge[0]);
@@ -242,21 +210,26 @@ function timeline() {
         curCharge[2] = Math.min(sw_max[2] * sw[2], curCharge[2]);
     }
 
-    while (currentPoint < 3000 || eventlist.length < 30) {
+    var stop = 0
+    while (eventlist.length < 20 && stop > 10 && currentPoint < 5000) {
         nextPoint = nextTimePoint();
         if (nextPoint == currentPoint) {
             settleSkill();
+            stop += 1
         } else {
             curCharge[0] += (nextPoint - currentPoint) * skillacc[0];
             curCharge[1] += (nextPoint - currentPoint) * skillacc[1];
             curCharge[2] += (nextPoint - currentPoint) * skillacc[2];
             currentPoint = nextPoint;
+            stop = 0
         }
         settleCharge();
     }
 }
 
 function draw() {
+    sw.forEach(e => { if (e = 100000) { e = 0; } });
+
     document.querySelector("#U1").innerHTML = "";
     document.querySelector("#U2").innerHTML = "";
     document.querySelector("#U3").innerHTML = "";
@@ -275,27 +248,27 @@ function draw() {
     ];
     var overflow = [0, 0, 0];
 
-    eventlist.forEach(e => {
+    eventlist.forEach(e, i => {
         var white = document.createElement("div");
         var div = document.createElement("div");
-        if (lastEvent[e[0] - 1][0] !== null) {
-            if (e[1] - 100 > lastEvent[e[0] - 1][0]) {
-                white.style.height = String((e[1] - lastEvent[e[0] - 1][0]) / 5 - 20 - overflow[e[0] - 1]) + "px";
-                overflow[e[0] - 1] = 0;
+        if (lastEvent[e[0]][0] !== null) {
+            if (e[1] - 100 > lastEvent[e[0]][0]) {
+                white.style.height = (e[1] - lastEvent[e[0]][0]) / 5 - 20 - overflow[e[0]] + "px";
+                overflow[e[0]] = 0;
             } else {
-                lastEvent[e[0] - 1][1].innerHTML += "/" + String(e[1]);
-                lastEvent[e[0] - 1][1].style.height = String(Math.max(20, Number(lastEvent[e[0] - 1][1].style.height.slice(0, -2))) + (e[1] - lastEvent[e[0] - 1][0]) / 5) + "px";
-                overflow[e[0] - 1] += (e[1] - lastEvent[e[0] - 1][0]) / 5;
+                lastEvent[e[0]][1].innerHTML += "/" + e[1] + "(" + i + ")";
+                lastEvent[e[0]][1].style.height = Math.max(20, lastEvent[e[0]][1].style.height.slice(0, -2)) + (e[1] - lastEvent[e[0]][0]) / 5 + "px";
+                overflow[e[0]] += (e[1] - lastEvent[e[0]][0]) / 5;
                 return;
             }
         } else {
-            white.style.height = String(e[1] / 5) + "px";
+            white.style.height = e[1] / 5 + "px";
         }
-        div.innerHTML = e[1];
-        lastEvent[e[0] - 1] = [e[1], div];
+        div.innerHTML = e[1] + "(" + i + ")";
+        lastEvent[e[0]] = [e[1], div];
         white.style.backgroundColor = "transparent";
-        document.querySelector("#U" + String(e[0])).appendChild(white);
-        document.querySelector("#U" + String(e[0])).appendChild(div);
+        document.querySelector("#U" + (e[0] + 1)).appendChild(white);
+        document.querySelector("#U" + (e[0] + 1)).appendChild(div);
     })
 }
 
@@ -305,5 +278,5 @@ function drawTimeline() {
     update();
     timeline();
     draw();
-    console.log(String(Date.now() - startTime) + "ms");
+    console.log(Date.now() - startTime + "ms");
 }
